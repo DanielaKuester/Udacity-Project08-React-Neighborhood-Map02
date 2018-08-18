7f3e509dd349d371188f220a1765932b1333c213
Daniela Küster <daniela.kuester@gmx.net>
2018-08-18 11:37:33 +0200

    Add Map of Lyon France

--- src/Map.js
+++ src/Map.js
@@ -3,11 +3,24 @@ import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 
 class Map extends Component {
 
+    initialiseMap = () => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsYS1rdWVzdGVyIiwiYSI6ImNqam9jdjF1YTAxczQzcG9na3JrZmh2bnkifQ.zMFUT-fI5tsviUOPZsJxog';
+        const map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v10',
+            center: [4.841389, 45.758889],
+            zoom: 12
+        });
+    }
+
+    componentDidMount() {
+        this.initialiseMap()
+    }
+
     render() { 
         return(
             <div id="map" class="map-container">
                 {/* Initialise the map here*/}
-
             </div>
         );
     }
