35b4c6fcab21f53731fcbb620e828796445c1c1b
Daniela Küster <daniela.kuester@gmx.net>
2018-08-18 10:38:59 +0200

    Add empty Map component

---
+++ src/Map.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+class Map extends Component {
+
+    render() { 
+        return(
+            <div id="map" class="map-container">
+                {/* Initialise the map here*/}
+            </div>
+        );
+    }
+}
+ 
+export default Map;
