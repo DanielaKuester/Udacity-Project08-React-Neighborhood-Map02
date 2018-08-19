42c2278c7b8510f65fd713700a5c6ec5787c016e
Daniela Küster <daniela.kuester@gmx.net>
2018-08-19 19:05:05 +0200

    Refactor: Define functions in App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 import Map from './Map'
 import axios from 'axios'
 
@@ -38,6 +39,31 @@ class App extends Component {
             })
     }
 
+    /**
+     * I already tried beforehand to initialise the map with the help of a separate function.
+     * I used this torial by Elharony to initialise my map:
+     * https://www.youtube.com/watch?v=W5LhLZqj76s&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1&index=2
+     * I would like to use "const map = new mapboxgl.Map();" instead of "this.map" to create the map
+     * and "const marker = new mapboxgl.Marker({})" instead of "this.marker" to create a new marker.
+     */
+
+    initialiseMap = () => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsYS1rdWVzdGVyIiwiYSI6ImNqam9jdjF1YTAxczQzcG9na3JrZmh2bnkifQ.zMFUT-fI5tsviUOPZsJxog';
+        this.map = new mapboxgl.Map({
+            container: 'map',
+            style: 'mapbox://styles/mapbox/streets-v10',
+            center: [4.841389, 45.758889],
+            zoom: 12
+        });
+    }
+
+    addMarkers = () => {
+        // create the marker
+        this.marker = new mapboxgl.Marker({color: this.props.color})
+        .setLngLat([4.841389, 45.758889])
+        .addTo(this.map)
+    }
+
     componentDidMount() {
         this.getFoursquareVenues()
     }
@@ -48,6 +74,9 @@ class App extends Component {
                 <main>
                     <Map
                         color={this.state.color}
+                        venues={this.state.foursquareVenues}
+                        initialiseMap={this.initialiseMap}
+                        addMarkers={this.addMarkers}
                     />
                 </main>
             </div>
--- src/Map.js
+++ src/Map.js
@@ -3,37 +3,14 @@ import mapboxgl from 'mapbox-gl/dist/mapbox-gl.js'
 
 class Map extends Component {
 
-    /**
-     * I already tried beforehand to initialise the map with the help of a separate function.
-     * I used this torial by Elharony to initialise my map:
-     * https://www.youtube.com/watch?v=W5LhLZqj76s&list=PLgOB68PvvmWCGNn8UMTpcfQEiITzxEEA1&index=2
-     * I would like to use "const map = new mapboxgl.Map();" instead of "this.map" to create the map
-     * and "const marker = new mapboxgl.Marker({})" instead of "this.marker" to create a new marker.
-     */
-
-    initialiseMap = () => {
-        mapboxgl.accessToken = 'pk.eyJ1IjoiZGFuaWVsYS1rdWVzdGVyIiwiYSI6ImNqam9jdjF1YTAxczQzcG9na3JrZmh2bnkifQ.zMFUT-fI5tsviUOPZsJxog';
-        this.map = new mapboxgl.Map({
-            container: 'map',
-            style: 'mapbox://styles/mapbox/streets-v10',
-            center: [4.841389, 45.758889],
-            zoom: 12
-        });
-    }
-
-    addMarkers = () => {
-        // create the marker
-        this.marker = new mapboxgl.Marker({color: this.props.color})
-        .setLngLat([4.841389, 45.758889])
-        .addTo(this.map)
-    }
 
     componentDidMount() {
-        this.initialiseMap()
-        this.addMarkers()
+        this.props.initialiseMap()
+        this.props.addMarkers()
     }
 
     render() { 
+        console.log(this.props)
         return(
             <div id="map" className="map-container">
                 {/* Initialise the map here*/}
